87b689f9be7a7731db467c35bf949f242004dead
qBlackbirdp <blackbird9711@gmail.com>
2024-06-28 21:48:35 +0900

    게시물 등록 번호 증가 기능 추가

--- src/main/java/com/koreait/App.java
+++ src/main/java/com/koreait/App.java
@@ -11,25 +11,29 @@ public class App {
     }
 
     public void run() {
-        System.out.println("== motivation 실행 ==");
+        System.out.println("== motivation execution ==");
+
+        int lastId = 1;
+
         while (true) {
-            System.out.print("명령어) ");
+            System.out.print("commend) ");
             String cmd = sc.nextLine().trim();
 
             if (cmd.equals("exit")) {
-                System.out.println("== motivation 종료 ==");
+                System.out.println("== motivation end ==");
                 break;
             } else if (cmd.length() == 0) {
                 System.out.println("명령어를 입력해주세요.");
                 continue;
             }
 
+
             if (cmd.equals("add")) {
                 System.out.print("motivation :");
                 String motivation = sc.nextLine();
                 System.out.print("source :");
                 String source = sc.nextLine();
-                System.out.println("~번 motivation이 등록 되었습니다.");
+                System.out.printf("%d번 motivation 이 등록 되었습니다.\n", lastId);
             }
         }
 
